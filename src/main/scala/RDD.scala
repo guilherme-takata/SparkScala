6eb063ac3ed06ad810d727275b544e9b15d43083
guilherme-takata <guilherme.ohta@gmail.com>
2022-04-28 17:16:34 -0300

    Example of RDD code

---
+++ src/main/scala/RDD.scala
@@ -0,0 +1,29 @@
+import org.apache.spark._
+import org.apache.log4j._
+
+object RDD
+{
+  def main(args: Array[String]): Unit =
+    {
+      // Only prints warning if they are errors
+      Logger.getLogger("org").setLevel(Level.ERROR)
+
+      // Initiates the SparkContext using all cores of the machine, with the name RDDRatings
+      val sc = new SparkContext(master = "local[*]", appName = "RatingsCounter")
+
+      // Load the lines of the file into data of a RDD
+      val lines = sc.textFile("data/ml-100k/u.data")
+
+      // Splits each line to a string and splits it out with tabs, taking only the thrid field
+      val ratings = lines.map(x => x.split("\t")(2))
+
+      // Counts each time a rating occurs
+      val results = ratings.countByValue()
+
+      // Sorts the ratings by their respective counts
+      val sortedResults = results.toSeq.sortBy(_._1)
+
+      sortedResults.foreach(println)
+    }
+}
+
